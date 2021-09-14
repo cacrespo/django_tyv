1e43d0ff6d2f23f502f1a74acd7b733f229c8094
Carlos A. Crespo <lvccrespo@gmail.com>
2021-09-14 14:00:43 -0300

    DecimalField() y productos "destacado" / "principal"
    Ajustamos campos precio y nueva categoría de productos

--- negocio/models.py
+++ negocio/models.py
@@ -15,8 +15,9 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=200)
     marca = models.CharField(max_length=100)
     tamano = models.CharField(max_length=4)
+    principal = models.BooleanField(default=False)
     destacado = models.BooleanField(default=False)
-    precio_unitario =  models.FloatField(default= 0.0)
+    precio_unitario =  models.DecimalField(default= 0.0)
 
 
     def guardar(self):
@@ -29,6 +30,6 @@ class Producto(models.Model):
 class Venta(models.Model):
     item = models.ForeignKey(Producto, on_delete = models.CASCADE)
     cantidad = models.IntegerField(default = 0, null = True, blank = True)
-    precio = models.FloatField(default= 0.0)
+    precio = models.DecimalField(default= 0.0)
     fecha = models.TimeField(timezone.now)
 
