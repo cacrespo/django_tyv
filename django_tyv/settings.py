b2c990a6864a462b7662f2c5d27c5a5e2145c9ae
Carlos A. Crespo <lvccrespo@gmail.com>
2022-05-07 22:47:05 -0300

    timezone

--- django_tyv/settings.py
+++ django_tyv/settings.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 import os
 
-# Para cargar los archivos .env
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
